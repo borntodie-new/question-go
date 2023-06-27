f0d77000ab0d76b68a6311245fe487d4a37c55a7
borntodie-new <2631937454@qq.com>
2023-06-27 20:48:25 +0800

    项目配置文件

---
+++ internal/config/config.go
@@ -0,0 +1,36 @@
+package util
+
+import (
+	"github.com/spf13/viper"
+	"time"
+)
+
+// Config stores all configuration of the application.
+// The values are read by viper from a config file or environment variables
+type Config struct {
+	DBDriver             string        `mapstructure:"DB_DRIVER"`
+	DBSource             string        `mapstructure:"DB_SOURCE"`
+	ServerAddress        string        `mapstructure:"SERVER_ADDRESS"`
+	TokenSymmetricKey    string        `mapstructure:"TOKEN_SYMMETRIC"`
+	AccessTokenDuration  time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
+	RefreshTokenDuration time.Duration `mapstructure:"REFRESH_TOKEN_DURATION"`
+}
+
+// LoadConfig reads configuration from file or environment variables
+func LoadConfig(path string) (config Config, err error) {
+	// set config file path
+	viper.AddConfigPath(path)
+	// set config file name
+	viper.SetConfigName("app")
+	// set config file type
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+	err = viper.Unmarshal(&config)
+	return
+}
